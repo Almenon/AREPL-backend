e12019fbe422175a14f93bd351f2716853001204
almenon <almenon214@gmail.com>
2019-05-18 13:10:53 -0700

    add test for encoding error

--- eval.test.ts
+++ eval.test.ts
@@ -33,6 +33,16 @@ suite("PythonEvaluator Tests", () => {
         pyEvaluator.execCode(input)
     })
 
+    test("no encoding errors with utf16", function(done){
+        pyEvaluator.onResult = (result)=>{
+            assert.equal(result.userError, '')
+            assert.equal(result.internalError, null)
+            done()
+        }
+        input.evalCode = "#㍦"
+        pyEvaluator.execCode(input)
+    })
+
     test("dump returns result", function(done){
         let gotDump = false
         pyEvaluator.onResult = (result)=>{
