3b5333e6244e0147eeb670ce11bcdca598a61f5c
almenon <almenon214@gmail.com>
2020-01-11 10:03:07 -0800

    split overloads into seperate file

--- python/python_evaluator.py
+++ python/python_evaluator.py
@@ -10,14 +10,16 @@ import os
 from sys import path, modules, argv, version_info, exc_info
 from contextlib import contextmanager
 from module_logic import get_non_user_modules
-import inspect
 
+# do NOT use from overloads import arepl_input_iterator
+# it will recreate arepl_input_iterator and we need the original
+import overloads
+from pickler import specialVars, pickle_user_vars, pickle_user_error
 from saved import get_starting_locals
 from saved import get_eval_locals
 from saved import copy_saved_imports_to_exec
 from saved import starting_locals
 from saved import arepl_store
-from pickler import specialVars, pickle_user_vars, pickle_user_error
 from user_error import UserError
 
 if util.find_spec("howdoi") is not None:
@@ -90,95 +92,9 @@ class ExecArgs(object):
 nonUserModules = get_non_user_modules()
 origModules = frozenset(modules)
 
-# AREPL-vscode does not support stdin yet so help breaks it
-# by overridding help with a non-stdin version we can prevent AREPL-vscode from freezing up
-# just a temp fix untill AREPL-vscode supports stdin
-
-
-def help_overload(arg=None):
-    if arg is None:
-        print(
-            """Welcome to python! :)
-If this is your first time using Python, you should definitely check out
-the tutorial on the Internet at https://docs.python.org/3.7/tutorial/.
-
-AREPL uses a custom implementation of help which does not have all the features of the interpreter help. 
-But AREPL's help can still give you information on functions / modules / objects you pass into it."""
-        )
-    else:
-        print(arg.__doc__)
-
-
-arepl_input_iterator = None
-
-
-def input_overload(prompt=None):
-    """AREPL requires standard_input to be hardcoded, like so: standard_input = 'hello world'; print(input()). You can also hardcode standard_input as a generator or list.
-    
-    Keyword Arguments:
-        prompt {str} --  (default: {None})
-    
-    Raises:
-        StopIteration -- if there is no more input
-    
-    Returns:
-        str
-    """
-    global arepl_input_iterator
-
-    if prompt is not None:
-        print(prompt)
-    try:
-        if arepl_input_iterator is not None:
-            try:
-                return next(arepl_input_iterator)
-            except StopIteration:
-                # give simple user-friendly error, we dont want users worry about error in arepl source code
-                raise StopIteration("There is no more input") from None
-        else:
-
-            callingFrame = inspect.currentframe().f_back
-            standard_input = callingFrame.f_globals["standard_input"]
-
-            if type(standard_input) is str:
-                arepl_input_iterator = iter([line for line in standard_input.split("\n")])
-            elif callable(standard_input):
-                arepl_input_iterator = standard_input()
-            else:
-                arepl_input_iterator = iter([line for line in standard_input])
-
-            return next(arepl_input_iterator)
-    except KeyError:
-        print("AREPL requires standard_input to be hardcoded, like so: standard_input = 'hello world'; print(input())")
-
-
-def howdoi_wrapper(strArg):
-    """howdoi is meant to be called from the command line - this wrapper lets it be called programatically
-    
-    Arguments:
-        strArg {str} -- search term
-    """
-
-    if strArg.lower() == "use arepl" or strArg.lower() == "arepl":
-        returnVal = "using AREPL is simple - just start coding and arepl will show you the final state of your variables. For more help see https://github.com/Almenon/AREPL-vscode/wiki"
-    else:
-        try:
-            parser = howdoi.get_parser()
-        except NameError as e:
-            # alter error to be more readable by user
-            e.args = ["howdoi is not installed"]
-            raise
-
-        args = vars(parser.parse_args(strArg.split(" ")))
-        returnVal = howdoi.howdoi(args)
-
-    print(returnVal)
-    return returnVal  # not actually necessary but nice for unit testing
-
-
-starting_locals["help"] = help_overload
-starting_locals["input"] = input_overload
-starting_locals["howdoi"] = howdoi_wrapper
+starting_locals["help"] = overloads.help_overload
+starting_locals["input"] = overloads.input_overload
+starting_locals["howdoi"] = overloads.howdoi_wrapper
 
 eval_locals = deepcopy(starting_locals)
 
@@ -223,7 +139,6 @@ def exec_input(codeToExec, savedLines="", filePath="", usePreviousVariables=Fals
     returns info about the executed code (local vars, errors, and timing)
     :rtype: returnInfo
     """
-    global arepl_input_iterator
     global arepl_store
     global eval_locals
 
@@ -283,7 +198,7 @@ def exec_input(codeToExec, savedLines="", filePath="", usePreviousVariables=Fals
                     pass  # it's not worth failing AREPL over
 
             # clear mock stdin for next run
-            arepl_input_iterator = None
+            overloads.arepl_input_iterator = None
 
     if showGlobalVars:
         userVariables = pickle_user_vars(eval_locals)
