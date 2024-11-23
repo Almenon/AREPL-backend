32009acaf5cd835b4dc554abe2f1a55f8c9635ac
almenon <almenon214@gmail.com>
2024-11-23 15:35:30 -0800

    fix spacing

--- pythonExecutors.ts
+++ pythonExecutors.ts
@@ -9,103 +9,103 @@ export * from './pythonExecutor'
  * as a single executor.
  */
 export class PythonExecutors {
-    private executors: PythonExecutor[] = []
-    private currentExecutorIndex: number = 0
-    private waitForFreeExecutor: NodeJS.Timeout
-    private wait_for_other_runs_to_complete: NodeJS.Timeout
+	private executors: PythonExecutor[] = []
+	private currentExecutorIndex: number = 0
+	private waitForFreeExecutor: NodeJS.Timeout
+	private wait_for_other_runs_to_complete: NodeJS.Timeout
 
-    constructor(public options: Options = {}){}
+	constructor(public options: Options = {}){}
 
-    start(numExecutors=3){
-        // we default to three executors, as it should be enough so that there is always
-        // one available to accept incoming code
+	start(numExecutors=3){
+		// we default to three executors, as it should be enough so that there is always
+		// one available to accept incoming code
 
-        if(this.executors.length != 0) throw Error('already started!')
+		if(this.executors.length != 0) throw Error('already started!')
 
-        for(let i = 0; i < numExecutors; i++){
-            console.log('starting executor ' + i.toString())
-            const pyExecutor = new PythonExecutor(this.options)
-            pyExecutor.start(()=>{})
-            pyExecutor.evaluatorName = i.toString()
-            pyExecutor.onResult = result => {
-                // Other executor may send a result right before it dies
-                // So we use this function to only capture result from active executor
-                if(i == this.currentExecutorIndex) this.onResult(result)
-            }
-            pyExecutor.onPrint = print => {
-                if(i == this.currentExecutorIndex) this.onPrint(print)
-            }
-            pyExecutor.onStderr = stderr => {
-                if(i == this.currentExecutorIndex) this.onStderr(stderr)
-            }
-            pyExecutor.pyshell.on('error', this.onError)
-            pyExecutor.pyshell.childProcess.on('exit', exitCode => {
-                if(exitCode != 0) this.onAbnormalExit(exitCode)
-            })
-            this.executors.push(pyExecutor)
-        }
-    }
+		for(let i = 0; i < numExecutors; i++){
+			console.log('starting executor ' + i.toString())
+			const pyExecutor = new PythonExecutor(this.options)
+			pyExecutor.start(()=>{})
+			pyExecutor.evaluatorName = i.toString()
+			pyExecutor.onResult = result => {
+				// Other executor may send a result right before it dies
+				// So we use this function to only capture result from active executor
+				if(i == this.currentExecutorIndex) this.onResult(result)
+			}
+			pyExecutor.onPrint = print => {
+				if(i == this.currentExecutorIndex) this.onPrint(print)
+			}
+			pyExecutor.onStderr = stderr => {
+				if(i == this.currentExecutorIndex) this.onStderr(stderr)
+			}
+			pyExecutor.pyshell.on('error', this.onError)
+			pyExecutor.pyshell.childProcess.on('exit', exitCode => {
+				if(exitCode != 0) this.onAbnormalExit(exitCode)
+			})
+			this.executors.push(pyExecutor)
+		}
+	}
 
-    /**
-     * Sends code to the current executor. 
-     * If current executor is busy, nothing happens
-     */
-    execCodeCurrent(code: ExecArgs){
-        this.executors[this.currentExecutorIndex].execCode(code)
-    }
+	/**
+	 * Sends code to the current executor. 
+	 * If current executor is busy, nothing happens
+	 */
+	execCodeCurrent(code: ExecArgs){
+		this.executors[this.currentExecutorIndex].execCode(code)
+	}
 
-    /**
-     * sends code to a free executor to be executed
-     * Side-effect: restarts dirty executors
-     */
-    execCode(code: ExecArgs){
-        // old code is now irrelevant, if we are still waiting to send old code
-        // we should stop waiting
-        clearInterval(this.waitForFreeExecutor)
-        // this timeout should definitely not still be going on, but we clear it just in case
-        clearTimeout(this.wait_for_other_runs_to_complete)
+	/**
+	 * sends code to a free executor to be executed
+	 * Side-effect: restarts dirty executors
+	 */
+	execCode(code: ExecArgs){
+		// old code is now irrelevant, if we are still waiting to send old code
+		// we should stop waiting
+		clearInterval(this.waitForFreeExecutor)
+		// this timeout should definitely not still be going on, but we clear it just in case
+		clearTimeout(this.wait_for_other_runs_to_complete)
 
-        let last_run_still_executing = false
-        if(this.executors.some(executor => executor.state == PythonState.Executing)){
-            last_run_still_executing = true
-        }
-        // executors running old code are now irrelevant, restart them
-        this.executors.filter(executor => executor.state == PythonState.Executing || executor.state == PythonState.DirtyFree)
-            .forEach(executor => executor.restart())
+		let last_run_still_executing = false
+		if(this.executors.some(executor => executor.state == PythonState.Executing)){
+			last_run_still_executing = true
+		}
+		// executors running old code are now irrelevant, restart them
+		this.executors.filter(executor => executor.state == PythonState.Executing || executor.state == PythonState.DirtyFree)
+			.forEach(executor => executor.restart())
 
-        if(last_run_still_executing){
-            // wait for last run to complete
-            // we don't want to run two programs at once
-            // which could cause a race condition
-            this.wait_for_other_runs_to_complete = setTimeout(this.exec_when_free_executor.bind(this, code), PythonExecutor.GRACE_PERIOD+5)
-        } else{
-            this.exec_when_free_executor(code)
-        }
-    }
+		if(last_run_still_executing){
+			// wait for last run to complete
+			// we don't want to run two programs at once
+			// which could cause a race condition
+			this.wait_for_other_runs_to_complete = setTimeout(this.exec_when_free_executor.bind(this, code), PythonExecutor.GRACE_PERIOD+5)
+		} else{
+			this.exec_when_free_executor(code)
+		}
+	}
 
-    private exec_when_free_executor(code: ExecArgs){
-        let freeExecutor = this.executors.find(executor=>executor.state == PythonState.FreshFree)
-        if(!freeExecutor){
-            this.waitForFreeExecutor = setInterval(()=>{
-                freeExecutor = this.executors.find(executor=>executor.state == PythonState.FreshFree)
-                if(freeExecutor){
-                    freeExecutor.execCode(code)
-                    this.currentExecutorIndex = parseInt(freeExecutor.evaluatorName)
-                    clearInterval(this.waitForFreeExecutor)
-                }
-            }, 60)
-        }
-        else{
-            freeExecutor.execCode(code)
-            this.currentExecutorIndex = parseInt(freeExecutor.evaluatorName)
-        }
-    }
+	private exec_when_free_executor(code: ExecArgs){
+		let freeExecutor = this.executors.find(executor=>executor.state == PythonState.FreshFree)
+		if(!freeExecutor){
+			this.waitForFreeExecutor = setInterval(()=>{
+				freeExecutor = this.executors.find(executor=>executor.state == PythonState.FreshFree)
+				if(freeExecutor){
+					freeExecutor.execCode(code)
+					this.currentExecutorIndex = parseInt(freeExecutor.evaluatorName)
+					clearInterval(this.waitForFreeExecutor)
+				}
+			}, 60)
+		}
+		else{
+			freeExecutor.execCode(code)
+			this.currentExecutorIndex = parseInt(freeExecutor.evaluatorName)
+		}
+	}
 
-    stop(kill_immediately=false){
-        clearInterval(this.waitForFreeExecutor)
-        this.executors.forEach(executor => executor.stop(kill_immediately))
-        this.executors = []
-    }
+	stop(kill_immediately=false){
+		clearInterval(this.waitForFreeExecutor)
+		this.executors.forEach(executor => executor.stop(kill_immediately))
+		this.executors = []
+	}
 
 	/**
 	 * checks syntax without executing code
@@ -116,7 +116,7 @@ export class PythonExecutors {
 		return PythonShell.checkSyntax(code);
 	}
 
-    /**
+	/**
 	 * Overwrite this with your own handler.
 	 * is called when active executor fails or completes
 	 */
@@ -139,14 +139,14 @@ export class PythonExecutors {
 	/**
 	 * Overwrite this with your own handler. 
 	 * Is called when there is a Node.JS error event with the python process
-     *  The 'error' event is emitted whenever:
-            The process could not be spawned, or
-            The process could not be killed, or
-            Sending a message to the child process failed.
+	 *  The 'error' event is emitted whenever:
+			The process could not be spawned, or
+			The process could not be killed, or
+			Sending a message to the child process failed.
 	 */
 	onError(err: NodeJS.ErrnoException) { }
 
-    onAbnormalExit(exitCode: number) {}
+	onAbnormalExit(exitCode: number) {}
 
 	/**
 	 * delays execution of function by ms milliseconds, resetting clock every time it is called
